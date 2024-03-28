6c81c6c56277eaa75dc044bf7b8dc43251a17763
Evangeline Ferrell <38958921+ejferr@users.noreply.github.com>
2024-03-28 15:28:04 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import * as Amplify from 'aws-amplify';
-import { withAuthenticator, useAuthenticator } from '@aws-amplify/ui-react';
-import React, { useState, useEffect } from 'react';
+import Amplify from '@aws-amplify/core';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 import awsconfig from './aws-exports';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'semantic-ui-less/semantic.less';
 import Ccp from './components/ccp';
 
 // Component
-function App() {
+function App({ signOut, user }) {
   const [isConfigured, setIsConfigured] = useState(false);
-  const { user } = useAuthenticator((context) => [context.user]);
 
   useEffect(() => {
     configureAuth();
   }, []);
 
   const configureAuth = () => {
-    Amplify.configure(awsconfig);
+    Amplify.default.configure(awsconfig);
     setIsConfigured(true);
   };
 
   return (
     <div className="App">
-      {isConfigured && <Ccp user={user} />}
+      {isConfigured && <Ccp user={user} signOut={signOut} />}
     </div>
   );
 }
