c4be65cc5cfd0f8d2949fc9472a1e376de8a81eb
Evangeline Ferrell <38958921+ejferr@users.noreply.github.com>
2024-03-29 18:10:46 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Amplify }  from '@aws-amplify/core';
 import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import awsconfig from './aws-exports';
 import React, { useState, useEffect } from 'react';
 import './App.css';
