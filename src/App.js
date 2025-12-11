717d5debec528c3d60a7eaa0d653e59f7744aeb9
Leonard Molano <lmolano@bridgewatercg.com>
2025-12-11 03:16:02 -0400

    Removed cognito integration from the application

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { Amplify }  from '@aws-amplify/core';
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
 import awsconfig from './aws-exports';
 import React, { useState, useEffect } from 'react';
 import './App.css';
@@ -9,7 +7,7 @@ import Ccp from './components/ccp';
 //import { autoSignIn } from '@aws-amplify/auth';
 
 // Component
-function App({ signOut, user }) {
+function App(/* { signOut, user } */) {
   const [isConfigured, setIsConfigured] = useState(false);
   
   useEffect(() => {
@@ -27,9 +25,9 @@ function App({ signOut, user }) {
 
   return (
     <div className="App">
-      {isConfigured && <Ccp user={user} signOut={signOut} />}
+      {isConfigured && <Ccp /* user={user} signOut={signOut} */ />}
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
