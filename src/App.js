858eade665b9b11c1b6ff4b8b241bb6430d6ecd3
Leonard Molano <lmolano@bridgewatercg.com>
2025-12-11 03:35:27 -0400

    Adding CONNECT_LOGIN_URL

--- src/App.js
+++ src/App.js
@@ -9,9 +9,17 @@ import Ccp from './components/ccp';
 // Component
 function App(/* { signOut, user } */) {
   const [isConfigured, setIsConfigured] = useState(false);
+  // Provide the Amazon Connect login/CCP URL via environment variable:
+  // REACT_APP_CONNECT_CCP_URL=https://your-instance.my.connect.aws/connect/ccp-v2
+  const CONNECT_LOGIN_URL = process.env.REACT_APP_CONNECT_CCP_URL;
   
   useEffect(() => {
     configureAuth();
+    // If a Connect login URL is provided, redirect to it (replace history)
+    if (CONNECT_LOGIN_URL) {
+      window.location.replace(CONNECT_LOGIN_URL);
+      return;
+    }
     //signedIn();
   }, []);
 
@@ -25,7 +33,14 @@ function App(/* { signOut, user } */) {
 
   return (
     <div className="App">
-      {isConfigured && <Ccp /* user={user} signOut={signOut} */ />}
+      {CONNECT_LOGIN_URL ? (
+        <div>
+          <p>Redirecting to Amazon Connect login...</p>
+          <p><a href={CONNECT_LOGIN_URL}>Go to Amazon Connect login</a></p>
+        </div>
+      ) : (
+        isConfigured && <Ccp /* user={user} signOut={signOut} */ />
+      )}
     </div>
   );
 }
