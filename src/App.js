0caad97c26ac226e1169246c003107cbd104d637
Leonard Molano <lmolano@bridgewatercg.com>
2026-01-19 23:29:57 -0400

    Removing redirect

--- src/App.js
+++ src/App.js
@@ -9,17 +9,9 @@ import Ccp from './components/ccp';
 // Component
 function App(/* { signOut, user } */) {
   const [isConfigured, setIsConfigured] = useState(false);
-  // Provide the Amazon Connect login/CCP URL via environment variable:
-  // REACT_APP_CONNECT_CCP_URL=https://your-instance.my.connect.aws/connect/ccp-v2
-  const CONNECT_LOGIN_URL = process.env.REACT_APP_CONNECT_CCP_URL;
   
   useEffect(() => {
     configureAuth();
-    // If a Connect login URL is provided, redirect to it (replace history)
-    if (CONNECT_LOGIN_URL) {
-      window.location.replace(CONNECT_LOGIN_URL);
-      return;
-    }
     //signedIn();
   }, []);
 
@@ -33,14 +25,7 @@ function App(/* { signOut, user } */) {
 
   return (
     <div className="App">
-      {CONNECT_LOGIN_URL ? (
-        <div>
-          <p>Redirecting to Amazon Connect login...</p>
-          <p><a href={CONNECT_LOGIN_URL}>Go to Amazon Connect login</a></p>
-        </div>
-      ) : (
-        isConfigured && <Ccp /* user={user} signOut={signOut} */ />
-      )}
+      {isConfigured && <Ccp /* user={user} signOut={signOut} */ />}
     </div>
   );
 }
